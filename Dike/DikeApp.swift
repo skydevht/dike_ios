b77f3d1b430729b46807faef559620c629a64ad9
Holy-Elie Scaide <skydhash@gmail.com>
2021-11-26 07:37:03 -0500

    Initial Commit

---
+++ Dike/DikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DikeApp.swift
+//  Dike
+//
+//  Created by Holy-Elie Scaide on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
